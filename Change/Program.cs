8b18f888b3f9bbb307c740bc5173d9227ce2208c
Marko Kosović <marko@kaloncreative.com>
2024-08-21 14:56:53 +0200

    Initial commit.

---
+++ Change/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// implement an algorithm for returning the change for a given amount of money and the price of the product
+// var change = GetChange(5, 4.20); 
+// the algorithm should return the minimum number of coins needed to return the change
+// the coins available are 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2
+// the algorithm should return the change in the following format: {coin: number of coins}
+
+
+// write code here
+
+Console.ReadLine();
